a6f3d050010c36b15125504fd032747e874619eb
Aaron Russell <russell.aaron94@gmail.com>
2015-09-20 14:57:02 -0600

    Reorganizing files

---
+++ www/app/app.module.js
@@ -0,0 +1,28 @@
+
+(function() {
+  
+  'use strict';
+  
+  angular
+  
+    .module('app', [
+      // List all of the app's dependencies
+      'ionic'
+    ])
+    
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory
+        // bar above the keyboard for form inputs)
+        if(window.cordova && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if(window.StatusBar) {
+          StatusBar.styleDefault();
+        }
+      });
+    })
+    
+  ;
+  
+})();
