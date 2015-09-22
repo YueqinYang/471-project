1dfe842fc72e2fdebca8c7cac23147b61cc5382d
Aaron Russell <russell.aaron94@gmail.com>
2015-09-21 22:36:19 -0600

    Sketched up inital view with a simple example

--- www/app/app.module.js
+++ www/app/app.module.js
@@ -4,16 +4,36 @@
   'use strict';
   
   angular
-  
+    
+    // This module is the top-level module for the project. In it, we'll
+    // include sub-modules that we create. Usually, each view get's it's own
+    // module.
     .module('app', [
       // List all of the app's dependencies
+      'example',
       'ionic'
     ])
     
+    // In this example, the module config establishes what states (or pages)
+    // exist within this module.
+    .config(function($stateProvider, $urlRouterProvider) {
+      
+      $stateProvider
+      
+        .state('example', {
+          controller: 'ExampleCtrl as vm',
+          templateUrl: 'app/example/example.tpl.html',
+          url: '/example'
+        })
+        
+      // If none of the above states are matched, use this as the fallback.
+      $urlRouterProvider.otherwise('/example');
+      
+    })
+    
+    // This establishes a few settings for Ionic
     .run(function($ionicPlatform) {
       $ionicPlatform.ready(function() {
-        // Hide the accessory bar by default (remove this to show the accessory
-        // bar above the keyboard for form inputs)
         if(window.cordova && window.cordova.plugins.Keyboard) {
           cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
